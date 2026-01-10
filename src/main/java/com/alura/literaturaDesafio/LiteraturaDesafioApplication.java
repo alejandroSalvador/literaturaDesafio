3bc0cd0c60460e63f3ddd0ad2b659ed3f4d2d0a3
Alejandro Castro <alejandro.castromoraga@outlook.com>
2026-01-10 14:48:31 -0300

    Primer archivo del desafio literatura descargardo de springboot

---
+++ src/main/java/com/alura/literaturaDesafio/LiteraturaDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literaturaDesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaDesafioApplication.class, args);
+	}
+
+}
